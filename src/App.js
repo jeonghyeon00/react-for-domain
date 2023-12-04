d825ad0f34c8aae849ee79f1d74435348cb7508e
jeonghyeon00 <sjh00kr@naver.com>
2023-12-04 15:15:07 +0900

    feat: Lotto Number Generator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+
+function generateLottoNumbers() {
+  let numbers = [];
+  while (numbers.length < 6) {
+    let num = Math.floor(Math.random() * 45) + 1;
+    if (!numbers.includes(num)) {
+      numbers.push(num);
+    }
+  }
+  return numbers;
+}
+
 
 function App() {
-  return <h1>Dockerfile.site</h1>;
+  const [numbers, setNumbers] = useState([]);
+  const handleGenerate = () => {
+    setNumbers(generateLottoNumbers());
+  };
+  return (
+    <div className="App">
+      <h1>Dockerfile.site</h1>
+      <header className="App-header">
+        <h1>Lotto Number Generator</h1>
+        <button onClick={handleGenerate}>Generate Numbers</button>
+        <div>
+          {numbers.sort((a,b)=>a-b).map((number, index) => (
+            <span key={index}>{number} </span>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
